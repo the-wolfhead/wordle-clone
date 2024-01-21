cec18d37fd0d13707b601b69a3b6016b44cfccb9
Daniel Otabor <danieldamianotabor@gmail.com>
2024-01-21 10:22:29 +0100

    Update Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,23 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-export default function Modal({isCorrect, turn, solution}) {
+const Modal = ({ isCorrect, turn, solution, onTryAgain }) => {
   return (
     <div className="modal">
-        {isCorrect && (
-            <div>
-                <h1>You Win</h1>
-                <p className="solution">{solution}</p>
-                <p>You found the solution in {turn} guesses :)</p>
-            </div>
-        )}
-        {!isCorrect && (
-            <div>
-                <h1>Nevermind!</h1>
-                <p className="solution">{solution}</p>
-                <p>Better luck next time :)</p>
-            </div>
-        )}
+      {isCorrect ? (
+        <div>
+          <h1>You Win</h1>
+          <p className="solution">{solution}</p>
+          <p>You found the solution in {turn} guesses :)</p>
+        </div>
+      ) : (
+        <div>
+          <h1>Nevermind!</h1>
+          <p className="solution">{solution}</p>
+          <p>Better luck next time :)</p>
+          <button onClick={onTryAgain}>Try Again</button>
+        </div>
+      )}
     </div>
-  )
-}
- 
+  );
+};
+
+export default Modal;
