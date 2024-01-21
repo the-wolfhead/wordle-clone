10108d8b827cfaee80a254d6bfa1ef578d37c5af
Daniel Otabor <danieldamianotabor@gmail.com>
2024-01-21 02:04:42 +0100

    Update Wordle.js

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,37 +1,37 @@
-import {useEffect, useState} from 'react'
-import useWordle from '../hooks/useWordle'
-import Grid from './Grid'
-import Keypad from './Keypad'
-import Modal from './Modal'
+import { useEffect, useState } from 'react';
+import useWordle from '../hooks/useWordle';
+import Grid from './Grid';
+import Keypad from './Keypad';
+import Modal from './Modal';
 
-export default function Wordle({solution}) {
-    const {currentGuess, handleKeyup, guesses, isCorrect, usedKeys, turn } = useWordle(solution)
-    const [showModal, setShowModal] = useState(false)
+export default function Wordle({ solution }) {
+  const { currentGuess, handleInputChange, handleEnterPress, guesses, isCorrect, usedKeys, turn } = useWordle(solution);
+  const [showModal, setShowModal] = useState(false);
 
-    useEffect(() => {
-        window.addEventListener('keyup', handleKeyup)
-
-        if (isCorrect) {
-          setTimeout(() => setShowModal(true), 2000)
-          window.removeEventListener('keyup', handleKeyup)
-        }
-
-        if(turn > 5) {
-          setTimeout(() => setShowModal(true), 2000)
-          window.removeEventListener('keyup', handleKeyup)
-        }
-
-        return () => window.removeEventListener('keyup', handleKeyup)
-    }, [handleKeyup, isCorrect, turn])
-
-    
+  useEffect(() => {
+    if (isCorrect || turn > 5) {
+      setTimeout(() => setShowModal(true), 2000);
+    }
+  }, [isCorrect, turn]);
 
   return (
     <div>
-        <div>current Guess - {currentGuess}</div>
-        <Grid currentGuess = {currentGuess} guesses = {guesses} turn = {turn}/>
-        <Keypad usedKeys={usedKeys} />
-        {showModal && <Modal isCorrect={isCorrect} turn={turn} solution={solution} />}
+      <div>
+        <input
+          type="text"
+          value={currentGuess}
+          maxLength={5}
+          onChange={handleInputChange}
+          onKeyPress={(e) => {
+            if (e.key === 'Enter') {
+              handleEnterPress();
+            }
+          }}
+        />
+      </div>
+      <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
+      <Keypad usedKeys={usedKeys} />
+      {showModal && <Modal isCorrect={isCorrect} turn={turn} solution={solution} />}
     </div>
-  )
+  );
 }
